4028ab230ae018f48b7beca98ce7b24c98dd4f04
Axel <axel.treinsoutrot@gmail.com>
2021-12-29 20:23:26 +0100

    first commit

---
+++ paramètre.c
@@ -0,0 +1,165 @@
+#include "paramètre.h"
+
+
+bool is_methode(char* chaine,char* methode){
+  for(int i=0;i<(int)strlen(methode);i++){
+    if(chaine[i]!=methode[i]) return false;
+  }
+  return true;
+}
+
+void do_balise(char balise, char* next_chaine, b_o *g_para,FILE *logfp){
+    if (balise == 'i'){
+      if (g_para -> b_i == true || g_para -> b_d == true){
+        printf("erreur il y a soit une balise en double soit 2 balise incompatible\n");
+        exit(EXIT_FAILURE);
+      }
+      g_para -> b_i =true;
+      if (is_specified_fichier(next_chaine,"csv") && exist(next_chaine) && access_lecture(next_chaine)){
+        g_para -> opt_i = next_chaine;
+        printf("balise -i OK\n ");
+        printf("opt_i: %s\n",g_para -> opt_i);
+        }
+        else{
+          printf("erreur argument invalide -i\n");
+          exit(EXIT_FAILURE);
+        }
+        }
+
+
+    if (balise == 'd'){
+      if (g_para -> b_i == true || g_para -> b_d == true){
+        printf("erreur il y a soit une balise en double soit 2 balise incompatible\n");
+        exit(EXIT_FAILURE);
+      }
+      g_para -> b_d =true;
+      if (is_specified_fichier(next_chaine,"csv") && exist(next_chaine) && access_lecture(next_chaine)){
+        g_para -> opt_d = next_chaine;
+        printf("balise -d OK\n ");
+        printf("opt_d: %s\n",g_para -> opt_d);
+      }
+      else{
+        printf("erreur argument invalide -d\n");
+        exit(EXIT_FAILURE);
+      }
+      }
+
+    if (balise == 'o'){
+    if (g_para -> b_o == true){
+      printf("erreur il y a dejà une balise -o\n");
+      exit(EXIT_FAILURE);
+    }
+    g_para -> b_o =true;
+    if (is_specified_fichier(next_chaine,"txt") && exist(next_chaine) && access_lecture(next_chaine) && access_write(next_chaine) && access_exec(next_chaine)){
+        g_para -> opt_o = next_chaine;
+        printf("balise -o OK\n ");
+        printf("opt_o: %s\n",g_para -> opt_o);
+        logfp=fopen("test.txt","a+");
+        fprintf(logfp,"ok -o\n");
+    }
+    else{
+      printf("erreur argument invalide -o\n");
+      exit(EXIT_FAILURE);
+    }
+    }
+
+    if (balise == 'm'){
+    if (g_para -> b_m == true){
+      printf("erreur il y a dejà une balise -m\n");
+      exit(EXIT_FAILURE);
+    }
+    g_para -> b_m =true;
+
+      if ( g_para -> b_d == true && (next_chaine == "uni1"|| next_chaine == "uni2" ) ){
+        printf("la balise -d est présente donc la méthode ne peut pas être uni1 ou uni2\n");
+        exit(EXIT_FAILURE);
+      }
+        g_para -> opt_m = next_chaine;
+        printf("balise -m OK\n ");
+        printf("opt_m: %s\n",g_para -> opt_m);
+  }
+}
+
+
+bool is_specified_fichier(char* chaine,char* extension){
+  int len_chaine=strlen(chaine), len_extension = strlen(extension),index_depart=len_chaine - len_extension;
+  int j=0;
+  for (int i = index_depart;i<index_depart+len_extension;i++){
+    if (chaine[i]!=extension[j]) return false;
+    j++;
+  }
+  return true;
+}
+
+bool exist(char* chaine){
+  if (access(chaine,F_OK)==0) return true;
+  return false;
+}
+
+bool access_lecture(char* chaine){
+  if (access(chaine,R_OK)==0) return true;
+  return false;
+}
+
+bool access_write(char* chaine){
+  if (access(chaine,W_OK)==0) return true;
+  return false;
+}
+
+bool access_exec(char* chaine){
+  if (access(chaine,X_OK)==0) return true;
+  return false;
+}
+
+
+int main(int argc,char **argv){
+  FILE *logfp;
+  logfp=stdout;
+  b_o g_para;
+  g_para.b_i=false;
+  g_para.b_d=false;
+  g_para.b_o=false;
+  g_para.b_m=false;
+  int c;
+  char* opta=NULL;
+  if (argc == 1){
+    printf("aucun paramètre n'est présent\n");
+    exit(EXIT_FAILURE);
+  }
+  else if (argc%2==0){
+    printf("il manque un paramètre\n");
+    exit(EXIT_FAILURE);
+  }
+  while ((c=getopt(argc,argv,"i:d:o:m:"))!=EOF){
+    switch(c){
+      case 'i':
+        do_balise((char) c,optarg,&g_para,logfp);
+        break;
+      case 'd':
+        do_balise((char) c,optarg,&g_para,logfp);
+        break;
+      case 'o':
+        do_balise((char) c,optarg,&g_para,logfp);
+        break;
+      case 'm':
+        if(is_methode(optarg,"uni1")) opta="uni1";
+        else if(is_methode(optarg,"uni2")) opta="uni2";
+        else if(is_methode(optarg,"cm")) opta="cm";
+        else if(is_methode(optarg,"cs")) opta="cs";
+        else if(is_methode(optarg,"all")) opta="all";
+        else{
+          printf("erreur argument invalide -m\n");
+          exit(EXIT_FAILURE);
+        }
+        do_balise((char) c,opta,&g_para,logfp);
+        break;
+      case '?':
+        exit(EXIT_FAILURE);
+        break;
+  }
+ }
+ if(g_para.b_i==false && g_para.b_d==false){
+   printf("il faut avoir la balise i ou d!\n");
+   exit(EXIT_FAILURE);
+ }
+}
---
+++ paramètre.h
@@ -0,0 +1,24 @@
+#ifndef PARAMETRE_H
+#define PARAMETRE_H
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct s_balise_option{
+  bool b_i,b_d,b_o,b_m;
+  char* opt_i;
+  char* opt_d;
+  char* opt_o;
+  char* opt_m;
+}b_o;
+
+void do_balise(char balise, char* next_chaine, b_o *g_para,FILE *logfp);
+bool is_specified_fichier(char* chaine,char* extension);
+bool exist(char* chaine);
+bool access_lecture(char* chaine);
+bool access_write(char* chaine);
+bool access_exec(char* chaine);
+bool is_methode(char* chaine,char* methode);
+#endif
